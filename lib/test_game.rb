716aeda26ffb5bdbf4863d66e532d90691090535
Nasim Ibrahimi <46283064+N-Ibrahimi@users.noreply.github.com>
2019-04-04 21:13:34 +0200

    game_finish
    pour tester le jeux et vérifier le code

---
+++ lib/test_game.rb
@@ -0,0 +1,29 @@
+
+def jeux_de_roi 
+x=0
+z=0
+while x<=9 && x>=0
+	y=rand(1...6)
+	if (y==5) || (y==6)
+		x+=1
+		puts "tu as fiat#{y}=> Tu as bien monte un etage; tu es au #{x}eme"
+	elsif (y==1) && (x<=0)
+		if x>0 
+		x=-1
+		puts "tu as fiat#{y}=> Tu es decendu d\'un etage; tu es au #{x}eme"
+		end  
+		puts "tu est tout en bas, fait un peu d'effort"
+		next 
+	else
+		puts "tu as fiat#{y}=> Tu bouge pas, donc tu es encore au #{x}eme"
+		x+=0
+	end
+		z+=1
+end   
+puts "Bravo tu es au 10eme etage"
+puts " tu as fait #{z} fois d'essaie"
+end 
+
+jeux_de_roi
+	
+
