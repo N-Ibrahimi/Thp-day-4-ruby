00c5893d8031ed723edda18902e346d23470c151
Nasim Ibrahimi <46283064+N-Ibrahimi@users.noreply.github.com>
2019-04-04 14:15:13 +0200

    Update 01_pyramids.rb

--- lib/01_pyramids.rb
+++ lib/01_pyramids.rb
@@ -17,4 +17,38 @@ def half_pyramids
   end 
 end 
 
-half_pyramids
+def full_pyramids
+
+  puts "Entrez combien d'etage tu veux?"
+  print ">  "
+  x = gets.chomp.to_i 
+  while x <= 26 && x >=0
+    etage= (x/2)+1
+    etage.times do |i|
+    i+=1
+    puts "#{ " "*(etage)}" + "#{("#"*i)}" + "#{"#"*(i-1)}"
+    etage-=1
+    end 
+
+    half= x/2
+    half.times do |y|
+    puts "#{" "*(y+2)}"+"#{"#"*(x-2)}"
+    y+=1
+    x-=2
+    end
+    break
+  end 
+  
+  def perform
+    half_pyramids
+    full_pyramids
+  end 
+  
+  perform 
+  
+    
+end 
+
+
+
+full_pyramids
