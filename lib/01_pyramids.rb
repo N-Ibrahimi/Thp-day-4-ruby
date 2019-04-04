84b87171caedc05edd9c8acec65d8742cc07b57c
Nasim Ibrahimi <46283064+N-Ibrahimi@users.noreply.github.com>
2019-04-04 21:31:00 +0200

    finish

--- lib/01_pyramids.rb
+++ lib/01_pyramids.rb
@@ -1,14 +1,17 @@
 def half_pyramids
+  puts "***********************************************************"
+  puts "**************     Half Pyramide        *******************"
+  puts "***********************************************************"
+  puts "                                                           "
+  puts "                                                           "
   puts "Salut, bienvenue dans ma super pyramide! Combien d'étages souahites tu? \n (choisis un nobmre entre 0 et 25)"
   print ">  "
 
-  
   etage= gets.chomp.to_i 
   if etage <= 26 && etage >=0
      etage.times do |i|
       i+=1
      puts "#{ " "*(etage)}" + "#{("#"*i)}" + "#{"#"*(i-1)}"
-     
      etage-=1
      end  
     else
@@ -17,8 +20,12 @@ def half_pyramids
 end 
 
 def full_pyramids
-
-  puts "Entrez combien d'etage tu veux?"
+  puts "***********************************************************"
+  puts "**************     Full Pyramide        *******************"
+  puts "***********************************************************"
+  puts "                                                           "
+  puts "                                                           "
+  puts "Salut, bienvenue dans ma super pyramide! Combien d'étages souahites tu? \n (choisis un nobmre entre 0 et 25)"
   print ">  "
   x = gets.chomp.to_i 
   while x <= 26 && x >=0
@@ -36,9 +43,23 @@ def full_pyramids
     x-=2
     end
     break
+  end  
+end 
+
+def continuation
+  puts "tu veux encore que je te cree les pyramide?(oui/non)"
+  print ">  " 
+  answer=gets.chomp 
+  if answer=="oui"
+      perform 
   end 
-      
 end 
 
 
+def perform 
+half_pyramids
 full_pyramids
+continuation
+end  
+perform 
+
